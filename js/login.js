4fc17ebc53b3dab81f057a236cadc7466c7c5d89
SirSKillz <lruhland@purdue.edu>
2019-04-16 23:41:13 -0400

    Adding error message and functions to collect from form

--- js/login.js
+++ js/login.js
@@ -1,1 +1,37 @@
+function sendLoginData() {
+    const logEl = document.getElementById("username").value;
+    const passEl = document.getElementById("password").value;
+    console.log(logEl, passEl);
+    onConfirmation();
+}
+
+function sendRegisterData() {
+    const logEl = document.getElementById("username").value;
+    const passEl = document.getElementById("password").value;
+    console.log(logEl, passEl);
+    onFailure();
+}
+
+function onConfirmation() {
+    window.location.href = "risk-game.html"
+}
+
+function onFailure() {
+    document.getElementById("errMsg").style.display = "block";
+}
+
+// Hide the login error message if the user clicks anywhere
+let clicks = 0;
+document.onclick = function () {
+    // Check if the error message is available
+    if (document.getElementById("errMsg").offsetParent !== null) {
+        if (clicks > 0) {
+            document.getElementById("errMsg").style.display = "none";
+            clicks = 0;
+        } else {
+            clicks++;
+        }
+    }
+};
+
 
