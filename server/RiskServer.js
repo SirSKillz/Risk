a229a0ffe9e012a53e9e0bd4c206d990568cb153
SirSKillz <lruhland@purdue.edu>
2019-04-18 17:13:01 -0400

    Working on getting server to connect

--- server/RiskServer.js
+++ server/RiskServer.js
@@ -1,25 +1,53 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var server;
 
-const risk = require('express')();
-//const path = require('path');
-const server = require('http').createServer(risk);
-const socket = require('socket.io')(server);
-const fs = require('fs');
+server = http.createServer(function(req, res){
+    // your normal server code
+    var path = url.parse(req.url).pathname;
+    console.log(path);
+    switch (path){
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write('<h1>Hello! Try the <a href="/login.html">Test page</a></h1>');
+            res.end();
+            break;
+        case '/login.html':
+            fs.readFile('..' + path, function(err, data){
+                if (err){
+                    return send404(res);
+                }
+                res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
+                res.write(data, 'utf8');
+                res.end();
+            });
+            break;
+        default: send404(res);
+    }
+}),
 
+    send404 = function(res){
+        res.writeHead(404);
+        res.write('404');
+        res.end();
+    };
 
-//risk.use(express.static(__dirname + '/node_modules'));
+server.listen(8001);
 
-risk.get('/', (req, res) => {
-    console.log('we in here');
-    res.writeHead(200, {"Content-type" : "text/css"});
-    let fileContents = fs.readFileSync('../css/login.css', {encoding: "utf8"});
-    res.write(fileContents);
-    res.end();
+// use socket.io
+var io = require('socket.io').listen(server);
+
+// define interactions with client
+io.sockets.on('connection', function(socket){
+    console.log("New user!");
+    //send data to client
+    setInterval(function(){
+        socket.emit('date', {'date': new Date()});
+    }, 1000);
+
+    //recieve client data
+    socket.on('client_data', function(data){
+        process.stdout.write(data.letter);
+    });
 });
-
-socket.on('connection', (data) =>{
-    console.log('client connected');
-});
-
-
-server.listen(63334);
-console.log('listening on port 63334');
