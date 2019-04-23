18f5c612088de1eec141d3fe6de75141ca500f99
rygoard <rgoard@purdue.edu>
2019-04-22 22:21:16 -0400

    basic communication with multiple clients

--- server/RiskServer.js
+++ server/RiskServer.js
@@ -1,8 +1,19 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-//const sql = require('sqlite3');
+const http    = require('http');
+const url     = require('url');
+const fs      = require('fs');
+const SQL   = require('sqlite3');
 
+
+global["database"] = new SQL.Database("./database.db", function (err) {
+    if (err) {
+        console.error(err.message);
+    } else {
+        // Create Login table
+        let createLoginSql = "CREATE TABLE IF NOT EXISTS login(username TEXT PRIMARY KEY, password TEXT)";
+        database.run(createLoginSql);
+        console.log("Database was successfully opened!");
+    }
+});
 
 server = http.createServer(function(req, res){
     // your normal server code
@@ -81,16 +92,43 @@ io.sockets.on('connection', function(socket){
         socket.emit('date', {'date': new Date()});
     }, 1000);
 
-    //recieve client data
-    socket.on('restart', function(data){
-        console.log("Trying to restart");
+    socket.on('login', function (username, password) {
+        let sql = "SELECT * FROM login WHERE username = ?";
+        database.get(sql, username, function (err, user) {
+            if (err) {
+                console.error(err.message);
+                socket.emit('badLogin');
+            } else {
+                // Check if the user exists and that the password is correct
+                if (user && user.password === password) {
+                    // The username and password match
+                    socket.emit('goodLogin');
+                } else {
+                    socket.emit('badLogin');
+                }
+            }
+        });
+    });
+    socket.on('register', function (username, password) {
+        let sql = "INSERT INTO login (username, password) VALUES (?, ?)";
+        database.run(sql, [username, password], function (err) {
+            if (err) {
+                socket.emit('usernameTaken');
+                console.warn("Username was taken: " + err);
+            } else {
+                // If it succeeded, it added a user
+                socket.emit('registerSuccess');
+                console.log("Registered new user: " + username);
+            }
+        });
     });
 
-    socket.on('loginRequest', function(user, pass){
-        console.log("Get login", user, pass);
-        if ((user === "ryan" && pass === "123") || (user === "noah" && pass === "123"))  {
-            console.log("Good login");
-            socket.emit('goodLogin');
-        }
+    socket.on('room', function() {
+        console.log('joining game 1');
+        socket.join('game 1');
     });
+
+    socket.on('start', function() {
+        socket.to('game 1').emit('start', 4);
+    })
 });
